ef6e97834a70b2e5ab9eeaae7c6863887d131ae9
Fengyi Liu <zhez@me.com>
2021-09-05 06:21:23 -0500

    Add start button and launch game activity

--- app/src/main/java/com/android/catchfruit/GameAcitivity.java
+++
@@ -1,4 +0,0 @@
-package com.android.catchfruit;
-
-public class GameAcitivity {
-}
--- app/src/main/java/com/android/catchfruit/MainActivity.java
+++ app/src/main/java/com/android/catchfruit/MainActivity.java
@@ -2,7 +2,10 @@ package com.android.catchfruit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view){
+        Intent gameIntent=new Intent(this,GameActivity.class);
+        startActivity(gameIntent);
     }
 }
